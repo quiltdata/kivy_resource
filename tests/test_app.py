2a71404d20ca4a3192b95ca2ec11e9bfc037d3dd
Ernest Prabhakar <ernest@quiltdata.io>
2023-01-03 15:22:24 -0800

    try non-integrated approach
    https://kivy.org/doc/stable/contribute-unittest.html?highlight=test

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,27 @@ from .conftest import *
 
 class AppTest(GraphicUnitTest):
 
-	def test_app(self):
+	def test_runtouchapp(self):
+		# non-integrated approach
+		from kivy.app import runTouchApp
+		from kivy.uix.button import Button
+
+		button = Button()
+		runTouchApp(button)
+
+		# get your Window instance safely
+		from kivy.base import EventLoop
+		EventLoop.ensure_window()
+		window = EventLoop.window
+
+		# your asserts
+		self.assertEqual(window.children[0], button)
+		self.assertEqual(
+			window.children[0].height,
+			window.height
+		)
+
+
+	def untest_app(self):
 		app = MainApp()
 		assert app
